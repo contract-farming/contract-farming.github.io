02efb360ae2d08efcd21d5761e873221c56200aa
hmes98318 <52697423+hmes98318@users.noreply.github.com>
2024-09-30 16:33:24 +0800

    feat: Update homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,20 +11,50 @@ function HomepageHeader() {
     return (
         <header className={clsx('', styles.heroBanner)}>
             <div className="container">
-                <h1 className="hero__title">契作農地</h1>
+                <h1 className="hero__title">農地契作管理平台文檔</h1>
                 <p className="hero__subtitle text--primary text--bold">
-                        這邊主要拿來放前後端安裝教學
-                        跟一些 DB 架構, 端口清單 API文檔
+                    這邊主要拿來放 API 文檔、 DB 架構跟一些前後端安裝教學
                 </p>
             </div>
         </header>
     );
 }
 
+function HomepageContent() {
+    return (
+        <section className="container">
+            <div className="markdown">
+                <h2>農地契作管理平台</h2>
+                <p>
+                    農地契作管理平台用於解決農作物生產過程中的追蹤與管理難題，我們與
+                    <a href="https://www.sinfang.tw/" target="_blank" rel="noopener noreferrer">新豐碾米工廠</a>合作，
+                    開發一套專屬的契作流程追蹤平台，結合
+                    <a href="https://maps.nlsc.gov.tw/" target="_blank" rel="noopener noreferrer">國土測繪地圖系統</a>，
+                    實現土地即時的耕作情況的可視化管理，幫助管理者隨時掌握作物的生長狀態與耕作過程，確保農民按標準施肥與施藥，
+                    並透過便捷的數據核對，快速完成產銷履歷的生成和記錄。
+                </p>
+                <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+                    <img
+                        src={require('./img/contract/contract-page.png').default}
+                        alt="Contract Management Page"
+                        style={{ width: '1080px', marginTop: '1rem' }}
+                    />
+                    <img
+                        src={require('./img/contract/contract-track.png').default}
+                        alt="Contract Management Page"
+                        style={{ width: '1080px', marginTop: '1rem', marginBottom: '10rem' }}
+                    />
+                </div>
+            </div>
+        </section>
+    );
+}
+
 export default function Home(): JSX.Element {
     return (
-        <Layout /*title={siteConfig.title} description={siteConfig.tagline}*/>
+        <Layout>
             <HomepageHeader />
+            <HomepageContent />
         </Layout>
     );
 }
